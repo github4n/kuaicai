5e7a4051fe149cf5ccea32fcbe08a4150fc7dd15
xtawgipl <xtawgpil1123@163.com>
2018-08-22 11:07:23 +0800

    提交

--- src/main/java/com/caipiao/job/OpenRecordJob.java
+++ src/main/java/com/caipiao/job/OpenRecordJob.java
@@ -66,64 +66,72 @@ public class OpenRecordJob extends QuartzJobBean {
             for(KuaiCai kuaiCai : data){
                 int row = kuaiCaiService.add(kuaiCai);
                 System.out.println(row);
-                if(row > 0){
-                    //生成新策略
-                    List<OmissionModel> omissionModels = kuaiCaiService.omissionTop(kuaiCaiService.listAll(false), 3, 10, false);
-                    for(OmissionModel omissionModel : omissionModels){
-                        if(omissionModel.getOmissionNum() > 120){
-                            Programme programme = programmeMapper.find(omissionModel.getCombination());
-                            if(programme == null){//还未加入计划
-                                Programme pp = new Programme();
-                                pp.setChasecode(omissionModel.getCombination());
-                                pp.setChasestart(omissionModel.getOmissionNum());
-                                pp.setExpect(kuaiCai.getExpect());
-                                pp.setProfit(0);
-                                pp.setState(0);
-                                pp.setStoploss(20);
-                                pp.setType(3);
-                                programmeMapper.insert(pp);
+                if(row > 0){//触发追号策略
+                    createStrategy(kuaiCai);//生成新策略
 
-                                Plan plan = Plan.getPre(pp.getId());
-                                for(int i =0; i < pp.getStoploss(); ++i){
-                                    plan = playStrategy.nextStep(plan);
-                                    planMapper.insert(plan);
-                                }
-                            }
-                        }
-                    }
-
-                    //计算旧策略
-                    List<Programme> programmes = programmeMapper.findAll();//所在进行中的策略
-                    for(Programme pro : programmes){
-                        String[] codes = pro.getChasecode().split(",");
-                        List<Plan> plans = planMapper.findByProgramme(pro.getId());
-                        Plan nextPlan = planMapper.findNextPlan(pro.getId());
-                        if(contains(kuaiCai.getOpencode(), Arrays.asList(codes))){//开出
-                            nextPlan.setExpect(kuaiCai.getExpect());
-                            nextPlan.setState(1);
-                            nextPlan.setOpencode(kuaiCai.getOpencode());
-                            planMapper.updateByPrimaryKey(nextPlan);
-
-                            pro.setState(1);
-                            pro.setProfit(nextPlan.getOpenamount() - nextPlan.getTotalamount());
-                            programmeMapper.updateByPrimaryKey(pro);
-                        }else{//未开出
-                            if(nextPlan == null){//止损
-                                pro.setState(2);
-                                pro.setProfit(nextPlan.getTotalamount());
-                                programmeMapper.updateByPrimaryKey(pro);
-                            }else{//生成下一步计划
-                                nextPlan.setExpect(kuaiCai.getExpect());
-                                nextPlan.setState(1);
-                                nextPlan.setOpencode(kuaiCai.getOpencode());
-                                planMapper.updateByPrimaryKey(nextPlan);
-                            }
-                        }
-                    }
+                    adjust(kuaiCai);//计算旧策略
                 }
             }
         } catch (RestClientException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void createStrategy(KuaiCai kuaiCai){
+        //生成新策略
+        List<OmissionModel> omissionModels = kuaiCaiService.omissionTop(kuaiCaiService.listAll(false), 3, 10, false);
+        for(OmissionModel omissionModel : omissionModels){
+            if(omissionModel.getOmissionNum() > 120){
+                Programme programme = programmeMapper.find(omissionModel.getCombination());
+                if(programme == null){//还未加入计划
+                    Programme pp = new Programme();
+                    pp.setChasecode(omissionModel.getCombination());
+                    pp.setChasestart(omissionModel.getOmissionNum());
+                    pp.setExpect(kuaiCai.getExpect());
+                    pp.setProfit(0);
+                    pp.setState(0);
+                    pp.setStoploss(20);
+                    pp.setType(3);
+                    programmeMapper.insert(pp);
+
+                    Plan plan = Plan.getPre(pp.getId());
+                    for(int i =0; i < pp.getStoploss(); ++i){
+                        plan = playStrategy.nextStep(plan);
+                        planMapper.insert(plan);
+                    }
+                }
+            }
+        }
+    }
+
+    private void adjust(KuaiCai kuaiCai){
+        //计算旧策略
+        List<Programme> programmes = programmeMapper.findAll();//所在进行中的策略
+        for(Programme pro : programmes){
+            String[] codes = pro.getChasecode().split(",");
+            List<Plan> plans = planMapper.findByProgramme(pro.getId());
+            Plan nextPlan = planMapper.findNextPlan(pro.getId());
+            if(contains(kuaiCai.getOpencode(), Arrays.asList(codes))){//开出
+                nextPlan.setExpect(kuaiCai.getExpect());
+                nextPlan.setState(1);
+                nextPlan.setOpencode(kuaiCai.getOpencode());
+                planMapper.updateByPrimaryKey(nextPlan);
+
+                pro.setState(1);
+                pro.setProfit(nextPlan.getOpenamount() - nextPlan.getTotalamount());
+                programmeMapper.updateByPrimaryKey(pro);
+            }else{//未开出
+                if(nextPlan == null){//止损
+                    pro.setState(2);
+                    pro.setProfit(nextPlan.getTotalamount());
+                    programmeMapper.updateByPrimaryKey(pro);
+                }else{//生成下一步计划
+                    nextPlan.setExpect(kuaiCai.getExpect());
+                    nextPlan.setState(1);
+                    nextPlan.setOpencode(kuaiCai.getOpencode());
+                    planMapper.updateByPrimaryKey(nextPlan);
+                }
+            }
         }
     }
 
